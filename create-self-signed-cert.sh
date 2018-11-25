044c772eab1497cae3b9464381cc3b0c8377aa43
Faisal Masood <fmasood@redhat.com>
2018-11-26 08:56:28 +1100

    intitial

---
+++ create-self-signed-cert.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# A bunch of openssl scripts to generate the certificate
+# forexternal master comms and a separate for internl comms.
+
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/security_guide/sec-using_openssl
+
+# stretch goal for https://blog.openshift.com/create-https-based-encrypted-urls-using-routes/
--- provision-ldap-node.sh
+++ provision-ldap-node.sh
@@ -2,6 +2,9 @@
 
 ##########
 # This script will provision one GCP node for LDAP
+# This ldap will be used to authenticate against OCP
+# We will also use some custom groups and map them to 
+# the OCP nodes.
 ##########
 
 set -e
@@ -11,4 +14,4 @@ gcloud config set project OCP-3-11
 gcloud compute instances create "ldap-server" --zone "asia-southeast1-a" --machine-type "n1-standard-2" --subnet "default" --maintenance-policy "TERMINATE" --service-account default --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_write" --image-project "rhel-cloud" --image "rhel-7-v20171129" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name "ldap-server" --tags "ldap-server" &
 
 # TODO -  add ldap provisioning here.
-# refer https://likegeeks.com/linux-ldap-server/
+# refer https://www.itzgeek.com/how-tos/linux/centos-how-tos/step-step-openldap-server-configuration-centos-7-rhel-7.html
