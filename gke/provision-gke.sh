74ba48942f6d00c93dbfc967350d997c1d606e52
Faisal Masood <a1225691@adelaide.edu.au>
2019-01-09 09:21:47 +1100

    added a gke folder

---
+++ gke/provision-gke.sh
@@ -0,0 +1,10 @@
+gcloud components update
+
+gcloud components install kubectl
+
+gcloud config set project [PROJECT_ID]
+gcloud config set compute/zone australia-southeast1
+
+#get the credentials of existing GKE cluster
+#gcloud container clusters get-credentials loadbalancedcluster
+
